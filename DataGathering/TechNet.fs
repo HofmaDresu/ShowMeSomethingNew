20484cacdb77b6ed76332fc3a31682e578506430
Matt Hoffman <hofmadresu@gmail.com>
2013-10-11 16:44:42 -0400

    Add DataGathering project. Start work for TechNet source

---
+++ DataGathering/TechNet.fs
@@ -0,0 +1,16 @@
+﻿namespace DataGathering
+
+open System.Net
+open FSharp.Data
+
+
+type msdnSchema = JsonProvider<"./SampleJson/TechNet.json">
+ 
+
+
+type TechNet() = 
+    let downloader = new WebClient()
+    let toc = downloader.DownloadString("http://technet.microsoft.com/en-us/library/bb510741.aspx?toc=1")
+    let technet = 
+        msdnSchema.Parse(toc)
+        |> Seq.filter (fun node -> node.ExtendedAttributes.DataTochassubtree)
