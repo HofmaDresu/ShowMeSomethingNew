c2da865e8fb9b5a1f879ba3984fdd51d9769091f
HofmaDresu <hofmadresu@gmail.com>
2013-10-12 17:36:41 -0400

    Add page to test technet call. Correct recursion for datagathering to track parent

--- DataGathering/Retriever.fs
+++ DataGathering/Retriever.fs
@@ -9,14 +9,20 @@ type msdnSchema = JsonProvider<"./SampleJson/TechNet.json">
  
 
 module Retriever =
-    let rec CollectTechNetSites(baseURI: string, relativeRootPage: string, title: string, hasParent: bool) = 
+    let rec private CollectTechNetSites(baseURI: string, relativeRootPage: string, title: string, parentID: int) = 
             let downloader = new WebClient()
             let toc = downloader.DownloadString(baseURI + relativeRootPage)
             let technetData = msdnSchema.Parse(toc)
             for datum in technetData do
                 //store
+                let myParent = -1
                 if datum.ExtendedAttributes.DataTochassubtree then
-                    CollectTechNetSites(baseURI, datum.Href, datum.Title, true)
-//        |> Seq.filter (fun node -> node.ExtendedAttributes.DataTochassubtree)
+                    CollectTechNetSites(baseURI, datum.Href + "?toc=1", datum.Title, myParent)
+
+                    
+    let CollectTechNetSite(baseURI: string, relativeRootPage: string, title: string) = 
+        //store
+        let myParent = -1
+        CollectTechNetSites(baseURI, relativeRootPage, title, myParent)
     
     
