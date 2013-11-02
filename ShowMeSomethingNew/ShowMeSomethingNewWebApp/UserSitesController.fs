d410d24f28cc27d66e72a05f464bc38f52377f6b
HofmaDresu <hofmadresu@gmail.com>
2013-11-01 20:09:39 -0400

    Create UserProgress view and service

--- ShowMeSomethingNew/ShowMeSomethingNewWebApp/UserSitesController.fs
+++ ShowMeSomethingNew/ShowMeSomethingNewWebApp/UserSitesController.fs
@@ -12,11 +12,12 @@ type UserSitesController() =
 
     // GET /api/values
     member x.Get(userName:string) = 
-        let foo = query {
-            for row in Retriever.db.TableOfContent do
-            select (row.SiteName, row.SiteId)
-            } 
-        foo 
+        let userProgress = query {
+            for up in Retriever.db.UserProgress do
+            where (up.UserName = userName)
+            select up
+        }
+        userProgress
     // @"[{""SiteName"":""test1"",""myCompletion"":""80%"",""averageCompletion"":""30%""},{""SiteName"":""test2"",""myCompletion"":""10%"",""averageCompletion"":""70%""}]"
     // GET /api/values/5
     member x.Get (userName:string, parentSiteId:int) = "http://www.google.com"
