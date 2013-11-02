cc9aca792b8f70ab4db6f67153e2547a6f2e298e
HofmaDresu <hofmadresu@gmail.com>
2013-11-01 21:04:01 -0400

    Create first version of get new page

--- ShowMeSomethingNew/ShowMeSomethingNewWebApp/UserSitesController.fs
+++ ShowMeSomethingNew/ShowMeSomethingNewWebApp/UserSitesController.fs
@@ -20,4 +20,20 @@ type UserSitesController() =
         userProgress
     // @"[{""SiteName"":""test1"",""myCompletion"":""80%"",""averageCompletion"":""30%""},{""SiteName"":""test2"",""myCompletion"":""10%"",""averageCompletion"":""70%""}]"
     // GET /api/values/5
-    member x.Get (userName:string, parentSiteId:int) = "http://www.google.com"
+    member x.Get (userName:string, parentSiteId:int) =
+        let newPageCount = query {
+            for unvisitedSite in Retriever.db.UnvisitedSitesForUser do
+            where (unvisitedSite.UserName = userName)
+            select unvisitedSite
+            count
+        } 
+        let newPage = query {
+            for unvisitedSite in Retriever.db.UnvisitedSitesForUser do
+            where (unvisitedSite.UserName = userName)
+            skip (System.Random().Next(0, newPageCount))
+            headOrDefault
+        } 
+        let newVisit = new dbSchema.ServiceTypes.UserVisits(UserId = newPage.UserId, SiteId = newPage.SiteId)
+        Retriever.db.UserVisits.InsertOnSubmit(newVisit);
+        Retriever.db.DataContext.SubmitChanges();
+        newPage.SiteName
